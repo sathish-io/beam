b13a6697e2f5efbf091ff0f4456016fdccbf2d03
Diego Ongaro <dongaro@ebay.com>
2017-10-17 15:06:46 -0700

    Add producer.sh script to add some data into Kafka

---
+++ producer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Lists working directory recursively and writes the output of the "file"
+# command for each file into the Kafka topic "beam". An example is:
+#     W{Key: "./some/file", Value: "ASCII text, with very long lines"}
+
+(
+  for f in $(find .); do
+    echo "W{Key: \"$f\", Value:\"$(file -b $f)\"}"
+  done
+) | bin/kafka-console-producer.sh --broker-list localhost:9092 --topic beam
